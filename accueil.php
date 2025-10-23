8328cd1b85cd93e6fae497f0c1864f6a32fa3ac6
unknown <donia.alfonsi@gmail.com>
2025-10-23 12:20:19 +0200

    Base du projet

---
+++ accueil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="logo/favori.svg" type="image/svg">
+    <title>GameSwipe</title>
+</head>
+<body>
+    <header>
+        <div class="top_bar">
+            <h1>GameSwipe</h1>
+            
+            <button class="button_bar">S'inscrire</button>
+            <button class="button_bar">Se connecter</button>
+
+            <button class="button_bar"><img src="logo/favori.svg" alt="favori"></button>
+            <button class="button_bar"><img src="logo/like.svg" alt="like"></button>
+            <button class="button_bar">
+                <img src="logo/like.svg" alt="like">
+                <img class="logo" src="logo/dislike.svg" alt="dislike">
+            </button>
+
+            <button class="button_bar">Filtres</button>
+        </div>
+    </header>
+    <div class="burger_bar">
+            <button class="burger"><img src="logo/burger_menu.svg" alt="berger menu"></button>
+    </div>
+    
+</body>
+</html>
