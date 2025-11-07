8a0c89c1e6ca3420ca5d8223e40f3763fae4171c
Caquite <john.smith@gmail.com>
2025-11-07 15:45:20 +0100

    Récupération de l'accueil pour tenter l'unification avec la carte

---
+++ accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="logo/favori.svg" type="image/svg">
+    <title>GameSwipe</title>
+
+    <style>
+        a{
+            color: inherit;
+            text-decoration: none;
+        }
+    </style>
+
+</head>
+<body>
+    <header>
+        <div class="top_bar">
+            <button class="gameswipe"><a href="accueil.php">GameSwipe</a></button>
+            
+            <button class="button_bar"><a href="inscription.html">S'inscrire</a></button>
+            <button class="button_bar"><a href="connecter.html">Se connecter</a></button>
+
+            <button class="button_bar"><img src="logo/favori.svg" alt="favori"></button>
+            <button class="button_bar"><img src="logo/like.svg" alt="like"></button>
+            <button class="button_bar">
+                <img src="logo/like.svg" alt="like">
+                <img class="logo" src="logo/dislike.svg" alt="dislike">
+            </button>
+
+            <button class="button_bar">Filtres</button>
+        </div>
+    </header>
+    <div class="burger_bar">
+            <button class="burger"><img src="logo/burger_menu.svg" alt="berger menu"></button>
+    </div>
+    
+</body>
+</html>
