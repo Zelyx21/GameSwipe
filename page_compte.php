e163f669fca69b9818c181c7222bfa13667375c5
Zelyx21 <beaulieu.clem.30260@gmail.com>
2025-11-19 17:45:25 +0100

    modifs

--- page_compte.php
+++ page_compte.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Profil</title>
 
