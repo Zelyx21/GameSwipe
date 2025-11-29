c7fc436acfc417199ee482ab8c8c36379546b6f5
fafidefafa <fafidefafa@gmail.com>
2025-11-29 22:58:07 +0100

    jai ajouter qlq notes

--- load_cards.php
+++ load_cards.php
@@ -5,10 +5,6 @@ require 'fonctions.php';
 $bdd = getBDD();
 
 $id_client = $_SESSION['client']['id'] ?? null;
-if (!$id_client) {
-    echo json_encode(["error" => "Utilisateur non connecté"]);
-    exit;
-}
 
 try {
     // Get user quiz answers
@@ -18,7 +14,7 @@ try {
 
     $q1 = $bdd->prepare("SELECT * FROM quest1 WHERE id_client = ?");
     $q1->execute([$id_client]);
-    $modes = $q1->fetch(PDO::FETCH_ASSOC);
+    $cats = $q1->fetch(PDO::FETCH_ASSOC);
 
     $q2 = $bdd->prepare("SELECT * FROM quest2 WHERE id_client = ?");
     $q2->execute([$id_client]);
@@ -27,7 +23,7 @@ try {
     $conditions = [];
     $params = [];
 
-    // age filters
+    // age
     if ($age) {
         if ($age['moins_de_13_ans'] == 1) {
             $conditions[] = "j.required_age <= 12";
@@ -41,15 +37,15 @@ try {
         }
     }
 
-    // genre filters
-    if ($modes) {
+    // category
+    if ($cats) {
         $catFilters = [];
 
-        if ($modes['solo'] == 1) $catFilters[] = 27;
-        if ($modes['coop'] == 1) $catFilters = array_merge($catFilters, [2,10,16,20,24,25]);
-        if ($modes['multi'] == 1) $catFilters = array_merge($catFilters, [15,11,17,19,26,4]);
-        if ($modes['mmo'] == 1) $catFilters[] = 12;
-        if ($modes['vr'] == 1) $catFilters = array_merge($catFilters, [35,37,38,39,36]);
+        if ($cats['solo'] == 1) $catFilters[] = 27;
+        if ($cats['coop'] == 1) $catFilters = array_merge($catFilters, [2,10,16,20,24,25]);
+        if ($cats['multi'] == 1) $catFilters = array_merge($catFilters, [15,11,17,19,26,4]);
+        if ($cats['mmo'] == 1) $catFilters[] = 12;
+        if ($cats['vr'] == 1) $catFilters = array_merge($catFilters, [35,37,38,39,36]);
 
         if (!empty($catFilters)) {
             $placeholders = implode(",", array_fill(0, count($catFilters), "?"));
@@ -58,7 +54,7 @@ try {
         }
     }
 
-    // playtime filters
+    // playtime
     if ($times) {
         $timeConditions = [];
         if ($times['2h'] == 1) $timeConditions[] = "j.avg_playtime_forever <= 120";
@@ -72,7 +68,7 @@ try {
         }
     }
 
-    // final query
+    // final requete sql
     $sql = "
         SELECT DISTINCT j.id_jeu, j.nom_jeu, j.image, j.description, j.release_date
         FROM jeux_videos j
